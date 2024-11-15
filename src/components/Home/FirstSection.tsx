ba91d25c3838109f977933dc4f0d5c01df7f5d2c
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2024-11-15 20:09:26 +0100

    feat: add first section

--- src/components/Home/FirstSection.tsx
+++ src/components/Home/FirstSection.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 const FirstSection = () => {
     return (
         <>
-            <div className='flex h-[50vh] bg-[#77B255]'></div>
+            <main className='h-[75vh] bg-[#0d214b]'>
+                <div className='ml-20 flex'>
+                    <h1 className='text-6xl font-bold text-white'>
+                        Waarom ons?
+                    </h1>
+                </div>
+            </main>
+            <div className='flex h-[100vh]'></div>
         </>
     )
 }
